df6596f20f0d38c1a808de437c8994a418e4204f
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-05-06 18:35:55 +0530

    Add files via upload

---
+++ day02/callback.js
@@ -0,0 +1,22 @@
+
+function login(callback){ //callback=validateUSer
+    callback() //validateUser
+    console.log("Vaild data")
+}
+
+function validateUser(callback){ //fetchData
+    callback()//fetchData()
+    console.log("Authorized")
+}
+
+function fetchData(){
+    console.log("Fetching")
+}
+
+// login(fetchData)
+// login(validateUser)
+
+//fetchthe data from external source -->validate data -->login functionality
+
+
+login(()=>validateUser(()=>fetchData())) //nestedcallback -callbackhell
